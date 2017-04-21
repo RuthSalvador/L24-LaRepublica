1721c9ea42746129efb04def5f15a93e597040c4
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-04-21 13:00:53 -0500

    esquema html e iconos x js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,33 @@
 window.addEventListener('load', function(){
 
-  
+  //function addPrincipal(){}
+  var icons = document.getElementsByClassName("icon-gris");
+  for (var i = 0; i < icons.length; i++) {
+    addIcons(icons[i]);
+  }
+
+  function addIcons(icons){
+    addClass("icon-facebook");
+    addClass("icon-twitter");
+    addClass("icon-share");
+
+    function addClass(clase){
+    var ulIco = document.createElement("ul");
+    icons.appendChild(ulIco);
+
+    var liIco = document.createElement("li");
+    ulIco.appendChild(liIco);
+
+    var span = document.createElement("span");
+    span.setAttribute("class", clase);
+    liIco.appendChild(span);
+    }
+  }
+
+
+
+
+
+
+
 })
